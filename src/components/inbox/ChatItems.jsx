68d27c8c90affdfe2d263ff7698e1e0d14a410fa
kayes360 <imrulkayes560@gmail.com>
2023-12-31 00:25:15 +0600

    chatboddy and sender, receiver msg printed

--- src/components/inbox/ChatItems.jsx
+++ src/components/inbox/ChatItems.jsx
@@ -3,7 +3,7 @@ import { useGetConversationsQuery } from "../../features/conversations/conversat
 import ChatItem from "./ChatItem";
 import Error from "../ui/Error";
 import moment from "moment";
-import getPartnerInfo from "../../utils/getPartnerInfo"
+import getPartnerInfo from "../../utils/getPartnerInfo";
 import gravatarUrl from "gravatar-url";
 import { Link } from "react-router-dom";
 export default function ChatItems() {
@@ -24,29 +24,33 @@ export default function ChatItems() {
   } else if (!isLoading && isError) {
     content = (
       <li className="m-2 text-center">
-        {" "}
         <Error message={error} />
       </li>
     );
   } else if (!isLoading && !isError && conversations?.length === 0) {
     content = <li className="m-2 text-center">No Conversations Found</li>;
+
   } else if (!isLoading && !isError && conversations?.length > 0) {
     content = conversations.map((conversation) => {
-        const {id, message, timestamp,users } = conversation
-        const {email} = user || {}
-        const {name, email: partnerEmail} = getPartnerInfo(conversation.users, email) 
+      const { id, message, timestamp, users } = conversation;
+      const { email } = user || {};
+      const { name, email: partnerEmail } = getPartnerInfo(
+        conversation.users,
+        email
+      );
 
       return (
+
         <li className="m-2 text-center" key={id}>
-          <Link to ={`/inbox/${id}`}>
-              <ChatItem 
-                avatar={gravatarUrl(partnerEmail, {
-                    size: 80
-                })}
-                name={name}
-                lastMessage={message}
-                lastTime={moment(timestamp).fromNow()}
-              />
+          <Link to={`/inbox/${id}`}>
+            <ChatItem
+              avatar={gravatarUrl(partnerEmail, {
+                size: 80,
+              })}
+              name={name}
+              lastMessage={message}
+              lastTime={moment(timestamp).fromNow()}
+            />
           </Link>
         </li>
       );
